21496cbc182d12c553bc93df1004d558bc676b7d
Jime Matthies <JimeMatthies@gmail.com>
2022-08-31 16:45:16 -0400

    Random Domain / app.js, index.html, style.css

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn");
+  document.querySelector(`#domain`).innerHTML = RandomDomain();
+  console.log("JM was here!");
+};
+
+let RandomDomain = () => {
+  let pron = ["the", "our", "my"];
+  let adj = ["great", "big", "awesome", "simple"];
+  let noun = ["jogger", "racoon", "vase", "teapot"];
+  let dot = [".com", ".net", ".org"];
+
+  let pronIndex = Math.floor(Math.random() * pron.length);
+  let adjIndex = Math.floor(Math.random() * adj.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+  let dotIndex = Math.floor(Math.random() * dot.length);
+  return pron[pronIndex] + adj[adjIndex] + noun[nounIndex] + dot[dotIndex];
 };
